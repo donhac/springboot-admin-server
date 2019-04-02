63ded1a82624b9e4327bf1e58a10a4a16e9d0753
donh <donhac@qq.com>
2019-04-02 11:46:34 +0800

    update sh

--- start-code.sh
+++ start-code.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 img_mvn="maven:3.3.3-jdk-8"                 # docker image of maven
 m2_cache=~/.m2                              # the local maven cache dir
 proj_home=$PWD                              # the project root dir
-img_output="deepexi/springcloud-eureka-server"         # output image tag
+img_output="deepexi/springboot-admin-server"         # output image tag
 
 git pull  # should use git clone https://name:pwd@xxx.git
 
@@ -14,17 +14,17 @@ echo "use docker maven"
         -v $proj_home:/usr/src/mymaven \
         -w /usr/src/mymaven $img_mvn mvn clean package -U
 
-sudo mv $proj_home/target/springcloud-eureka-server-*.jar $proj_home/target/demo.jar # 兼容所有sh脚本
+sudo mv $proj_home/target/springboot-admin-server-*.jar $proj_home/target/demo.jar # 兼容所有sh脚本
 docker build -t $img_output .
 
 # 删除容器
-docker rm -f springcloud-eureka-server &> /dev/null
+docker rm -f springboot-admin-server &> /dev/null
 
 # 启动镜像
 docker run -d --restart=on-failure:5 --privileged=true \
-    -p 8761:8761 \
+    -p 8769:8769 \
     -w /home \
-    --name springcloud-eureka-server deepexi/springcloud-eureka-server \
+    --name springboot-admin-server deepexi/springboot-admin-server \
     java \
         -Djava.security.egd=file:/dev/./urandom \
         -Duser.timezone=Asia/Shanghai \
@@ -32,5 +32,4 @@ docker run -d --restart=on-failure:5 --privileged=true \
         -XX:+PrintGCTimeStamps \
         -XX:+PrintGCDetails \
         -XX:+HeapDumpOnOutOfMemoryError \
-        -jar /home/demo.jar \
-          --eureka.instance.ip-address=192.168.31.100
+        -jar /home/demo.jar
